717a69841e783ff274ca1cac671aee266379160f
Bianca Sakaniwa <bih_sayuri@hotmail.com>
2022-09-05 18:17:11 -0300

    subindo o front!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EsqueciSenhaComponent } from './esqueci-senha/esqueci-senha.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'login/sign-in', redirectTo: 'sign-in', pathMatch: 'full' },
+  { path: 'senha', component: EsqueciSenhaComponent },
+  { path: 'login/esqueci-senha', redirectTo: 'senha', pathMatch: 'full' },
+  { path: 'feedback', component: FeedbackComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavegacaoComponent } from './navegacao/navegacao.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { EsqueciSenhaComponent } from './esqueci-senha/esqueci-senha.component';
+import { ConteudoComponent } from './conteudo/conteudo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavegacaoComponent,
+    FooterComponent,
+    NotFoundComponent,
+    LoginComponent,
+    WelcomeComponent,
+    SignInComponent,
+    FeedbackComponent,
+    EsqueciSenhaComponent,
+    ConteudoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
